75945b285998f02c63c2f96c53f66cd922eb677b
panic0021 <panic.veljko@gmail.com>
2025-05-24 00:53:30 +0200

    Added a certficate for HTTPS

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,6 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();
