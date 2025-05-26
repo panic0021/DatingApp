f3919530938ea35ace097c53a065f482787947c1
panic0021 <panic.veljko@gmail.com>
2025-05-27 01:51:39 +0200

    Added authentication and authorization (JWT Token)

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -1,14 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/users")] // /api/users
-
-    public class UsersController
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -16,12 +15,15 @@ namespace API.Controllers
             _context = context;
         }
 
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();
             return users;
         }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
